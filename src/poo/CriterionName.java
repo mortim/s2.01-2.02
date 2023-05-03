0f560b56a764ebbae632473c382104fa17f0a08a
AOULAD-TAYAB Karim <karim.aouladtayab.etu@univ-lille.fr>
2023-05-03 10:05:23 +0200

    Premier jet de l'enum CriterionName

--- src/poo/CriterionName.java
+++ src/poo/CriterionName.java
@@ -1,31 +1,20 @@
 package poo;
 
 public enum CriterionName {
-	GUEST_ANIMAL_ALLERGY,
-	HOST_HAS_ANIMAL,
-	GUEST_FOOD,
-	HOST_FOOD,
-	HOBBIES,
-	GENDER,
-	PAIR_GENDER,
-	HISTORY,
-	AGE_GAP;
+	GUEST_ANIMAL_ALLERGY('B'),
+	HOST_HAS_ANIMAL('B'),
+	GUEST_FOOD('T'),
+	HOST_FOO('T'),
+	HOBBIES('T'),
+	GENDER('T'),
+	PAIR_GENDER('T'),
+	HISTORY('T'),
+	AGE_GAP('N');
 	
 	private final char TYPE;
 	
-	private CriterionName() {
-		switch(this.name()) {
-			case "GUEST_ANIMAL_ALLERGY":
-			case "HOST_HAS_ANIMAL":
-				this.TYPE = 'B';
-				break;
-			case "AGE_GAP":
-				this.TYPE = 'N';
-				break;
-			default:
-				this.TYPE = 'T';
-				break;
-		}
+	private CriterionName(char type) {
+		this.TYPE = type;
 	}
 	
 	public char getType() {
