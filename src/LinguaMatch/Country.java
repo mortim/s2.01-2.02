66fb31023502d3446f5fd4c68c17b05db3368d21
Kylian Lagache <kylian.lagache.etu@univ-lille.fr>
2023-05-06 19:09:58 +0200

    Ajout de l'enum Country et documentation du package LinguaMatch

---
+++ src/LinguaMatch/Country.java
@@ -0,0 +1,12 @@
+package LinguaMatch;
+
+/**
+ * Définit tous les pays possibles pour un séjour linguistique
+ * @author WASSON Baptiste, LAGACHE Kylian, AOULAD-TAYAB Karim
+*/
+public enum Country {
+    FRANCE,
+    ITALY,
+    SPAIN,
+    GERMANY;
+}
