0352e3eee47fc210eb79ec40de5222801c0d0d26
Baptiste Wasson <baptiste.wasson.etu@univ-ille.fr>
2023-05-09 15:37:40 +0200

    Fix typo

--- src/LinguaMatch/package-info.java
+++ src/LinguaMatch/package-info.java
@@ -1,9 +1,9 @@
 /**
- * <p>LinguaMatch est une application (avec et sans interface graphique) permettant d'automatiser le processus d'apparaiement entre adolescents hôtes/visiteur dans le cadre de séjours linguistiques...</p>
+ * <p>LinguaMatch est une application (avec et sans interface graphique) permettant d'automatiser le processus d'appariement entre adolescents hôtes/visiteur dans le cadre de séjours linguistiques...</p>
  * <p>Avec LinguaMatch vous pourrez:</p>
  * <ul>
- *  <li>Importer un CSV avec un format bien précis contentant les informations et critères des adolescents et gérer les éventuelles incohérences</li>
- *  <li>Trouver une solution sur les meilleurs apparaiements possibles pour tous les adolescents (si possible) via des algorithmes d'affectation et pouvoir ainsi les afficher</li>
+ *  <li>Importer un fichier CSV avec un format bien précis contentant les informations et critères des adolescents et gérer les éventuelles incohérences</li>
+ *  <li>Trouver une solution sur les meilleurs appariements possibles pour tous les adolescents (si possible) via des algorithmes d'affectation et pouvoir ainsi les afficher</li>
  *  <li>Afficher les adolescents affectés nul part (toutes les contraines n'ont pas été satisfaites)
  *  <li>Prendre en compte un historique des séjours des années précédentes pour une meilleure affectation des hôtes-visiteurs
  * </ul>
